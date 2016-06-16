42afc9794e0beb6de03345e12945286732beedcb
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-06-16 15:12:54 -0700

    Added conditionals

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,24 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+var equal;
+
+if (cardTwo === cardFour){
+  equal = false;
+} else if (cardOne === cardTwo){
+  equal = true;
+} else if (cardThree === cardFour){
+  equal = true;
+} else if (cardOne === cardThree){
+  equal = false;
+} else if (cardOne === cardFour){
+  equal = false;
+} else if (cardTwo === cardThree){
+  equal = false;
+}
+
+if (equal === true) {
+  alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
